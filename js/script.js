0e226c1dc64d1f77ae7a4fa4d33e16b9abe0c2f6
basemw <basemwadi57@gmail.com>
2024-01-15 01:15:28 +0300

    Fixed Bug in API call, Added Dates, Added more blocked ids

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const filtered = [
   51534303, 253782, 65848252, 4328785, 25565916, 2859599, 2260470, 14033542,
   17333325, 4722129, 14229363, 14229363, 57295505, 4722129, 66942198, 14769870,
-  58900, 11520995, 14769870, 57463713, 33182870, 40395096, 58743047, 60288619,
+  58900, 11520995, 14769870, 57463713, 33182870, 40395096, 58743047, 60288619,26917287,
+  1347842, 62161607, 12252248, 144601514887, 27853106, 606717
 ];
 let numberOfPages;
 let page = 0;
@@ -22,9 +23,19 @@ function displayProducts(products) {
 
     const description = document.createElement("p");
     description.textContent = product.description;
-
+	
+	
+	const dateText = document.createElement("p");
+    if(product.natural_date)
+		dateText.textContent = product.natural_date;
+	else{
+		let date = new Date(product.record_posted_date_timestamp * 1000);
+		let formatted = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
+		dateText.textContent = formatted;
+	}
+	
     const price = document.createElement("p");
-    price.textContent = `Price: ${product.price} JOD`;
+    price.textContent = `${product.price} JOD`;
 
     const link = document.createElement("a");
     link.href = product.deeplink;
@@ -34,6 +45,7 @@ function displayProducts(products) {
     // Append elements to the product container
     productDiv.appendChild(img);
     productDiv.appendChild(name);
+    productDiv.appendChild(dateText);
     productDiv.appendChild(description);
     productDiv.appendChild(price);
     productDiv.appendChild(link);
@@ -48,7 +60,6 @@ const getData = () => {
   myHeaders.append("session-id", "30878133");
   myHeaders.append("Accept-Language", "ar");
   myHeaders.append("release-version", "9.4.02");
-  myHeaders.append("Access-Control-Allow-Origin", "*");
   myHeaders.append(
     "Authorization",
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdDAiOjE3MDMxNzgxMzQsImF1ZCI6ImRlc2t0b3AiLCJzdWIiOjMwODc4MTMzLCJybmQiOiI0NjEzMTUxNSIsImV4cCI6MTcwNTI0MTg2Nn0.pSibFuArB0CjtdI1poSkQb1TsnnEyuMErnZh7YrQogg"
@@ -61,8 +72,7 @@ const getData = () => {
   };
 
   fetch(
-    //`https://api.opensooq.com/v2.1/posts?fields=id,members_id,title,natural_date,price,display_name,deeplink,first_image_thumb,description,statusLabel&acceptDecimalPrice=1&supportCurrency=true&PostSearch[cityId]=59&PostSearch[neighborhood_id]=10886,6869,6851,6801,6759,6781,6863&PostSearch[categoryId]=8035&PostSearch[subCategoryId]=8037&PostSearch[dynamicAttributes][Surface][from]=101&PostSearch[dynamicAttributes][Surface][to]=175&PostSearch[dynamicAttributes][Surface][unit]=1&PostSearch[search_type]=all&PostSearch[priceFrom]=200&PostSearch[priceTo]=370&per-page=50&page=${page}&PostSearch[term]=`,
-	`https://jo.opensooq.com/v2.1/posts/search-by-ids?acceptDecimalPrice=1&supportCurrency=true&PostSearch[cityId]=59&PostSearch[neighborhood_id]=10886,6869,6851,6801,6759,6781,6863&PostSearch[categoryId]=8035&PostSearch[subCategoryId]=8037&PostSearch[dynamicAttributes][Surface][from]=101&PostSearch[dynamicAttributes][Surface][to]=175&PostSearch[search_type]=all&PostSearch[priceFrom]=200&PostSearch[priceTo]=370&per-page=50&page=${page}&PostSearch[term]=&PostSearch[dynamicAttributes][Furnished_RealEstate][0]=4729&fields=id,members_id,title,natural_date,price,display_name,deeplink,first_image_thumb,description,statusLabel`,
+    `https://api.opensooq.com/v2.1/posts?fields=id,record_posted_date_timestamp,members_id,title,natural_date,price,display_name,deeplink,first_image_thumb,description,statusLabel&acceptDecimalPrice=1&supportCurrency=true&PostSearch[cityId]=59&PostSearch[neighborhood_id]=10886,6869,6851,6801,6759,6781,6863&PostSearch[categoryId]=8035&PostSearch[subCategoryId]=8037&PostSearch[dynamicAttributes][Surface][from]=101&PostSearch[dynamicAttributes][Surface][to]=175&PostSearch[dynamicAttributes][Surface][unit]=1&PostSearch[search_type]=all&PostSearch[priceFrom]=200&PostSearch[priceTo]=370&per-page=50&page=${page}&PostSearch[term]=`,
     requestOptions
   )
     .then((response) => response.json())
